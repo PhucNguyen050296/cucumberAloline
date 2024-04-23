ffc71366333577063629c8a66b3cb7a41a3dbd9c
nguyen hong phuc <phucphuc25000025@gmail.com>
2024-04-23 22:09:21 +0700

    Ghi chú

---
+++ CucumberAloline/src/test/java/RunCucumberTests.java
@@ -0,0 +1,13 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "vn.aloline",
+        plugin = {"pretty", "html:target/cucumber-html-report.html"}
+)
+@Test
+public class RunCucumberTests extends AbstractTestNGCucumberTests {
+
+}
---
+++ CucumberAloline/src/test/java/aloline/StepsLoginCRM.java
@@ -0,0 +1,29 @@
+package aloline;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepsLoginCRM {
+    @And("I click the Login button")
+    public void iClickTheLoginButton() {
+    }
+
+    @And("I should see the {string} menu")
+    public void iShouldSeeTheMenu(String arg0) {
+
+    }
+
+    @Given("I am on the login page")
+    public void iAmOnTheLoginPage() {
+    }
+
+    @When("I enter my username and password")
+    public void iEnterMyUsernameAndPassword() {
+    }
+
+    @Then("I should be taken to the Dashboard page")
+    public void iShouldBeTakenToTheDashboardPage() {
+    }
+}
